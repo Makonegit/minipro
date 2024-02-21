df6d60b1bb7f63e0eb3ac72b5a998d2d88f1fa08
Makonegit <you@example.comvilaikoneslot@gmail.com>
2024-02-21 19:25:10 +0700

    first commitminipro

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello vue555555</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiniproApp',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
